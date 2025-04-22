428dbe6bc519b17649ba7698f1ddbe230bd94eed
Kaiser Mighty <unikmey@gmail.com>
2025-04-22 14:09:48 -0700

    Status Change

--- bot/bot.py
+++ bot/bot.py
@@ -7,6 +7,12 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
+    await bot.change_presence(
+        activity=discord.Activity(
+            type=discord.ActivityType.watching,
+            name="over you"
+        )
+    )
     print(f"Bot is ready. Logged in as {bot.user}")
 
 token = os.getenv("DISCORD_TOKEN")
